cad92c35d8a3847d2ab3f26326adeffe619a797e
mysoyul <vega2k_@naver.com>
2024-09-11 14:24:08 +0900

    FunctionAndThenCompose update

--- src/main/java/lambdasinaction/_01lambda/functional/FunctionAndThenCompose.java
+++ src/main/java/lambdasinaction/_01lambda/functional/FunctionAndThenCompose.java
@@ -1,5 +1,9 @@
 package lambdasinaction._01lambda.functional;
+import java.util.Comparator;
+import java.util.function.DoubleToIntFunction;
 import java.util.function.Function;
+import java.util.function.IntUnaryOperator;
+import java.util.function.ToIntBiFunction;
 
 public class FunctionAndThenCompose {
 
@@ -19,7 +23,12 @@ public class FunctionAndThenCompose {
 		Function<Integer,Integer> k = i.compose(j);
 		
 		System.out.println(k.apply(2)); //5
-		
+
+		ToIntBiFunction<Integer,Integer> intBiFunc = (x1, x2) -> x1 + x2;
+		intBiFunc = (x1, x2) -> Integer.sum(x1, x2);
+		intBiFunc = Integer::sum;
+
+		IntUnaryOperator intUnaryOp = x -> x + 1;
 	}
 
 }
