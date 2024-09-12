07ebe856541139a56ae25f52859e5f849a11d471
mysoyul <vega2k_@naver.com>
2024-09-12 11:44:12 +0900

    MobileTesterWithOptional update

--- src/main/java/lambdasinaction/_02stream/with/optional/MobileTesterWithOptional.java
+++ src/main/java/lambdasinaction/_02stream/with/optional/MobileTesterWithOptional.java
@@ -14,10 +14,24 @@ public class MobileTesterWithOptional {
 		int width = mService.getMobileScreenWidth(Optional.of(mobile));
 		System.out.println("Apple iPhone 6s Screen Width = " + width);
 
+		dfeatures.getResolution() //Optional<ScreenResolution>
+				.ifPresent(scr -> System.out.println(scr.getWidth()));
+
 		Mobile mobile2 = new Mobile(2015001, "Apple", "iPhone 6s", Optional.empty());		
 		int width2 = mService.getMobileScreenWidth(Optional.of(mobile2));
 		System.out.println("Apple iPhone 16s Screen Width = " + width2);
-		
+
+		Optional<DisplayFeatures> optionalDisplayFeatures = mobile2.getDisplayFeatures();
+		DisplayFeatures displayFeatures =
+				optionalDisplayFeatures.orElseGet(() -> new DisplayFeatures("0.0", Optional.empty()));
+		System.out.println(displayFeatures.getSize());
+		optionalDisplayFeatures.ifPresentOrElse(
+				df -> System.out.println(df.getSize()),
+				() -> System.out.println("optionalDisplayFeatures is null")
+		);
+
+		optionalDisplayFeatures.orElseThrow(() -> new RuntimeException("optionalDisplayFeatures not found!!"));
+
 	}
 
 }
