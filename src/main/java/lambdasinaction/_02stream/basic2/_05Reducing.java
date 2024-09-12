0baee52a56bf793caf79471a712af43775a85b5c
mysoyul <vega2k_@naver.com>
2024-09-12 09:36:28 +0900

    Reducing update

--- src/main/java/lambdasinaction/_02stream/basic2/_05Reducing.java
+++ src/main/java/lambdasinaction/_02stream/basic2/_05Reducing.java
@@ -34,13 +34,29 @@ public class _05Reducing {
                 .sum();
         System.out.println("sum3 = " + sum);
 
+        //1. Stream 의 reduce() 메서드 구현 - 최대값 구하기
+        int max = numbers.stream()
+                //.reduce(0, (n1,n2) -> Integer.max(n1,n2));
+                .reduce(0, Integer::max);
+        System.out.println("max = " + max);
+
+        //2. Stream 의 max() 메서드 사용 - 최대값 구하기
+        max = numbers.stream()
+                //max(Comparator)
+                //max((n1,n2) -> n1.compareTo(n2))
+                .max(Integer::compareTo) //Optional<Integer>
+                //.get();
+                .orElse(0);
+        System.out.println("Stream max = " + max);
+
         //reduce -  최소값
+        int min = numbers.stream()
+                .reduce(Integer::min) //Optional<Integer>
+                .orElse(0);
+        System.out.println("min = " + min);
 
-        //reduce - 최대값
-
-
-        Optional<Integer> min = numbers.stream().reduce(Integer::min);
-        min.ifPresent(System.out::println);
+        Optional<Integer> optional = numbers.stream().reduce(Integer::min);
+        optional.ifPresent(System.out::println);
 
         //Dish 의  총 칼로리 합계를 구하는 여러가지 방법
 
