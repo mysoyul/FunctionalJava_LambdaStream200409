7d4d19caa6a2ac49b16a53333732b2331f55e564
mysoyul <vega2k_@naver.com>
2024-09-12 09:29:43 +0900

    Reducing update

--- src/main/java/lambdasinaction/_02stream/basic2/_05Reducing.java
+++ src/main/java/lambdasinaction/_02stream/basic2/_05Reducing.java
@@ -3,6 +3,7 @@ package lambdasinaction._02stream.basic2;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 
 public class _05Reducing {
@@ -11,6 +12,27 @@ public class _05Reducing {
 
         //reduce - a + b 연산
         List<Integer> numbers = Arrays.asList(3, 4, 5, 1, 2);
+        //1. reduce() 메서드를 구현해서 합계 구하기
+        int sum = numbers.stream()
+                //.reduce(0, (n1,n2) -> n1 + n2);
+                .reduce(0, Integer::sum);
+        System.out.println("sum = " + sum);
+
+        //2. IntStream 의 sum() 사용해서 합계 구하기
+        sum = numbers.stream()
+                //mapToInt(ToIntFunction) int applyAsInt(T value);
+                //.mapToInt(value -> value.intValue()) //IntStream
+                .mapToInt(Integer::intValue)
+                .sum();
+        System.out.println("sum2 = " + sum);
+
+        //3. Stream 의 flatMapToInt() 생성한 IntStream 의 sum() 사용해서 합계 구하기
+        //IntStream flatMapToInt(Function<? super T,? extends IntStream> mapper)
+        sum = numbers.stream()
+                //.flatMapToInt(value -> IntStream.of(value))
+                .flatMapToInt(IntStream::of) //IntStream
+                .sum();
+        System.out.println("sum3 = " + sum);
 
         //reduce -  최소값
 
